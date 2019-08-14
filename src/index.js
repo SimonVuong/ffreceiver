8191af25ff827833448d691086bbe12c829880aa
Simon Vuong <simon.vuong@yahoo.com>
2019-08-14 04:30:35 -0400

    can print order and items async

--- src/index.js
+++ src/index.js
@@ -1,47 +1,106 @@
 import http from 'http';
-import { printer, types } from 'node-thermal-printer';
+import { printer } from 'node-thermal-printer';
 import { activeConfig } from './config';
+import PQueue from 'p-queue';
+
+const localPrinters = {};
+
+const getPrinterQ = ip => {
+  if (!localPrinters[ip]) {
+    localPrinters[ip] = new PQueue({ concurrency: 1 });
+  }
+  return localPrinters[ip];
+}
+
+const printItem = (ip, type, port, customerName, { name, selectedPrice, selectedOptions, quantity, specialRequests }) => {
+  const thermalPrinter = new printer({
+    interface: `tcp://${ip}:${port}`,
+    type,
+  });
+  thermalPrinter.println(`Customer: ${customerName}`);
+  thermalPrinter.newLine();
+  thermalPrinter.println(`Name: ${name} (${quantity})`)
+  thermalPrinter.newLine();
+
+  if (selectedPrice.label || selectedOptions) {
+    thermalPrinter.println(`Options:`);
+  }
+
+  if (selectedPrice.label) {
+    thermalPrinter.println(selectedPrice.label);
+    thermalPrinter.newLine();
+  }
+
+  if (selectedOptions) {
+    selectedOptions.forEach(({ name }) => thermalPrinter.println(name));
+    thermalPrinter.newLine();
+  }
+
+  if (specialRequests) thermalPrinter.println(`Special requests: ${specialRequests}`);
+  
+  thermalPrinter.cut();
+  thermalPrinter.beep();   
+  
+  return thermalPrinter.execute();
+}
+
+const printOrder = (ip, type, port, customerName, items, { itemTotal, tax, tip, total }) => {
+  const thermalPrinter = new printer({
+    interface: `tcp://${ip}:${port}`,
+    type,
+  });   
+  thermalPrinter.print(`Customer: ${customerName}`);
+
+  items.forEach(({ name, selectedPrice, selectedOptions, quantity, specialRequests }) => {
+    thermalPrinter.newLine()
+    thermalPrinter.leftRight(`${name} (${quantity})`, quantity*selectedPrice.value);
+    if (selectedPrice.label) {
+      thermalPrinter.println(`- ${selectedPrice.label}`);
+    }
+    if (selectedOptions) {
+      selectedOptions.forEach(({ name }) => thermalPrinter.println(`- ${name}`));
+    }
+    if (specialRequests) thermalPrinter.println(`Special requests: ${specialRequests}`);
+  });
+
+  thermalPrinter.leftRight('Item total', itemTotal);
+  thermalPrinter.leftRight('Tax', tax);
+  thermalPrinter.underline(true);
+  thermalPrinter.leftRight('Tip', tip);
+  thermalPrinter.underline(false);
+  thermalPrinter.leftRight('Total', total);
+
+  thermalPrinter.cut();
+  thermalPrinter.beep();   
+  return thermalPrinter.execute();
+}
 
 const print = printRequest => {
   const customerName = printRequest.customer;
+
   // print items
-  printRequest.data.items.forEach(({ name, selectedPrice, selectedOptions, quantity, specialRequests, printers }) => {
+  printRequest.data.items.forEach(({ printers, ...item }) => {
     printers.forEach(({ ip, type, port }) => {
-      console.log('printing to', ip, type, port);
-      console.log('item', { name, selectedPrice, selectedOptions, quantity, specialRequests })
-      const thermalPrinter = new printer({
-        interface: `tcp://${ip}:${port}`,
-        type,
+      const q = getPrinterQ(ip);
+      q.add(async () => {
+        try {
+          await printItem(ip, type, port, customerName, item);
+        } catch (e) {
+          console.log(`failed to print to ${ip}, ${e}`);
+        }
       });
-      thermalPrinter.println(`Customer: ${customerName}`);
-      thermalPrinter.newLine();
-      thermalPrinter.println(`Name: ${name} (${quantity})`)
-      thermalPrinter.newLine();
-      thermalPrinter.println(`Options:`);
+    });
+  });
 
-      if (selectedPrice.label) {
-        thermalPrinter.println(selectedPrice.label);
-        thermalPrinter.newLine();
+  // print order
+  printRequest.receiptPrinters.forEach(({ ip, type, port }) => {
+    const q = getPrinterQ(ip);
+    q.add(async () => {
+      try {
+        await printOrder(ip, type, port, customerName, printRequest.data.items, printRequest.data.costs);
+      } catch (e) {
+        console.log(`failed to print receipt to ${ip}, ${e}`);
       }
-
-      if (selectedOptions) {
-        selectedOptions.forEach(({ name }) => thermalPrinter.println(name));
-        thermalPrinter.newLine();
-      }
-
-      if (specialRequests) thermalPrinter.println(`Special requests: ${specialRequests}`);
-      
-      thermalPrinter.cut();
-      thermalPrinter.beep();   
-      
-      thermalPrinter.execute()
-        .then(res => {
-          console.log('print success:', res)
-        })
-        .catch(e => {
-          console.error('print err:', e);
-        });
-
     });
   });
 };
@@ -61,58 +120,23 @@ const registerReceiver = () => {
   http.get(options, res => {
     res.setEncoding('utf8');
     res.on('data', data => {
+      if (data === '1') {
+        console.log('received heartbeat');
+        return;
+      }
       try {
         const response = JSON.parse(data);
         print(response);
-      } catch(e) { // non json response
-	if (data === '1') {
-	  console.log('received heartbeat');
-	} else {
-	  console.log(`data: ${data}`);
-	}
+      } catch(e) {
+        console.log(`error ${e} with ${data}`);
       }
     });
     res.on('end', () => console.log(`Registration aborted`) )
   })
-  .on('close', c => console.log('closed'))
+  .on('close', () => console.log('closed'))
   .on('error', e => console.log('error', e));
 
   console.log(`listening with config ${JSON.stringify(activeConfig)}`);
 }
 
 registerReceiver();
-
-/**
- * 
- * 
- *   printOrder(signedInUser, cart, receiverId, receiptPrinters, itemTotal, tax, tip, total) {
-    const registeredReceiver = this.getRegisteredReceiver(receiverId);
-    registeredReceiver.write(JSON.stringify({
-      printers: receiptPrinters,
-      message: `Customer: ${signedInUser.email}`
-      + cart.items.map(({ name, selectedPrice, selectedOptions, quantity, specialRequests }) => (
-        '\n' + name + `(${quantity})${selectedPrice.label ? ' - ' +  selectedPrice.label : ''}` + ` ${selectedPrice.value}` 
-      )),
-    }));
-  }
-
-  printItems(signedInUser, rest, cart) {
-    const registeredReceiver = this.getRegisteredReceiver(rest.receiver.receiverId);
-    cart.items.forEach(({ name, categoryIndex, itemIndex, selectedPrice, selectedOptions, quantity, specialRequests }) => {
-      const storedItem = rest.menu[categoryIndex].items[itemIndex];
-      registeredReceiver.write(JSON.stringify({
-        printers: storedItem.printers,
-        data: {
-          customer: signedInUser.email,
-          item: {
-            name,
-            quantity,
-            selectedPrice,
-            selectedOptions,
-            specialRequests,
-          },
-        },
-      }));
-    })
-  }
- */
